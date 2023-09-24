391f8953a8fa3850d74a508060e41a19eaca18ce
joshuaburke117 <76673057+joshuaburke117@users.noreply.github.com>
2023-09-24 13:05:01 +1000

    adding junnit test files

---
+++ Web/WEB-INF/Advancedsoft/src/test/java/uts/bank/model/DAO/AccountDAOTest.java
@@ -0,0 +1,47 @@
+package uts.bank.model.DAO;
+
+import uts.bank.model.Account;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class AccountDAOTest {
+
+    private AccountDAO accountDAO;
+
+    @BeforeEach
+    public void setUp() {
+        accountDAO = new AccountDAO();
+    }
+
+    @Test
+    public void testAddAccount() throws SQLException {
+        // Create a sample account
+        Account account = new Account("test@email.com", "Test Account", "Savings", 1000.0, 1000.0);
+
+        // Call the addAccount method
+        accountDAO.addAccount(account);
+
+        // You can add additional assertions here to check the result of the operation if needed
+
+        // Clean up by deleting the test data (if necessary)
+        // Implement this based on your application's requirements
+    }
+
+    @Test
+    public void testFindAccounts() {
+        // Provide a sample customer email for testing
+        String customerEmail = "test";
+
+        // Call the findaccounts method
+        List<Account> accounts = accountDAO.findaccounts(customerEmail);
+
+        // Assert that the returned list is not null and contains at least one account
+        assertEquals(false, accounts.isEmpty());
+
+        // You can add additional assertions here to check the correctness of the retrieved accounts
+    }
+}
