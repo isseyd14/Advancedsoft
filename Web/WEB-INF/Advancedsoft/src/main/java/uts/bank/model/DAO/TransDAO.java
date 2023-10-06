51b0e88935b9c6361f5cc761fa31acfbbc10b30e
frog1025 <94098194+frog1025@users.noreply.github.com>
2023-10-06 11:40:06 +1100

    create mdb

---
+++ Web/WEB-INF/Advancedsoft/src/main/java/uts/bank/model/DAO/TransDAO.java
@@ -0,0 +1,17 @@
+package uts.bank.model.DAO;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.client.*;
+
+
+public class TransDAO{
+    String uri = "mongodb+srv://admin:admin@cluster0.2hlmjh5.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient mongoClient = MongoClients.create(uri);
+    MongoDatabase database = mongoClient.getDatabase("bank");
+    MongoCollection<Document> collection = database.getCollection("transaction");
+
+}
