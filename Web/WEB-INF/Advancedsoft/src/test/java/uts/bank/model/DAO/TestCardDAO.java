17a209ea934f6696bbb27af8c778239f70c27b1d
frog1025 <94098194+frog1025@users.noreply.github.com>
2023-10-08 16:25:02 +1100

    fix order annotation

--- Web/WEB-INF/Advancedsoft/src/test/java/uts/bank/model/DAO/TestCardDAO.java
+++ Web/WEB-INF/Advancedsoft/src/test/java/uts/bank/model/DAO/TestCardDAO.java
@@ -10,6 +10,7 @@ import uts.bank.model.Card;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCardDAO {
 
+
 //test methods in CardDAO.java
     static CardDAO aCardDAO = new CardDAO();
     static Card aCard;
@@ -37,16 +38,7 @@ public class TestCardDAO {
         System.out.println(actual.size());
         Assertions.assertEquals(1, actual.size());
     }
-    @Order(3)
-    @Test
-    //test findCardbycustomerid method
-    public void testFindCardByCustomerId() throws Exception {
 
-        List<Card> actual = aCardDAO.findCardByCustomerId("6001");
-        //System.out.println(expected);
-        System.out.println(actual.size());
-        Assertions.assertEquals(1, actual.size());
-    }
     @Order(4)
     @Test
     //test blockCard method
@@ -75,8 +67,7 @@ public class TestCardDAO {
         Assertions.assertEquals(expected, actual.getPin());
     }
 
-    @Order(7)
-    @Test
+    @AfterAll
     public static void tearDown() throws Exception {
         aCardDAO.deleteCard("5125763023039519");
     }
