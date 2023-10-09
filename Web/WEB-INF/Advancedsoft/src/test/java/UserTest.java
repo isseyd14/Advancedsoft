bc11f62aa06c24c739061d4566d9c0b21239f39f
isseyd14 <125661399+isseyd14@users.noreply.github.com>
2023-10-09 16:14:04 +1100

    ttest

--- Web/WEB-INF/Advancedsoft/src/test/java/UserTest.java
+++ Web/WEB-INF/Advancedsoft/src/test/java/UserTest.java
@@ -1,6 +1,6 @@
-package uts.bank.model.DAO;
 
 import org.junit.jupiter.api.*;
+import uts.bank.model.DAO.UserDAO;
 import uts.bank.model.user;
 import java.sql.Date;
 
@@ -13,26 +13,26 @@ import java.util.List;
 public class UserTest {
     static UserDAO testuserDao = new UserDAO();
 
-    static User testAccount;
+    static user testAccount;
 
     @BeforeAll
     public static void setUp(){
         Date specificSQLDate = Date.valueOf("2002-08-30");
 
-        testUser = new User("unit test email", "pass", "customer", "t", "t","t",specificSQLDate,"23233");
+        testAccount = new user("unit test email", "pass", "customer", "t", "t",specificSQLDate,"23233","t");
     }
     @Test
     public void testAddUser() throws SQLException {
         testuserDao.addUser(testAccount);
-        User Faccount = testuserDao.findUser(testUser);
+        user Faccount = testuserDao.findUser(testAccount.getEmail());
         // Check if the actual list contains the testAccount
         boolean userFound = false;
 
-            if (testAccount.getEmail.equals(Faccount)) {
+            if (testAccount.getEmail().equals(Faccount.getEmail())) {
                 userFound = true;
         }
         // Check if the account with the specified username was found in the list
-        Assertions.assertTrue(accountFound, "Account not found in the database.");
+        Assertions.assertTrue(userFound, "Account not found in the database.");
         // Check if there is only one account with the specified username
     }
     @AfterAll
