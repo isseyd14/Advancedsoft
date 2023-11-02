d0cdbd12da1606fade3a1dd96de7251eb72d9fd2
frog1025 <94098194+frog1025@users.noreply.github.com>
2023-11-02 23:24:30 +1100

    add search

--- Web/WEB-INF/Advancedsoft/src/main/java/uts/bank/model/DAO/TransDAO.java
+++ Web/WEB-INF/Advancedsoft/src/main/java/uts/bank/model/DAO/TransDAO.java
@@ -80,4 +80,28 @@ public class TransDAO{
         return trans;
     }
 
+    //search histroy
+    public List<Transaction> findTransByKeyword(String accountId, String keyword) {
+        List<Transaction> trans = new ArrayList<>();
+        String sql = "SELECT * FROM transaction WHERE account_id = ? and payee_email like ?";
+        try (Connection conn = getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);){
+            stmt.setString(1, accountId);
+            stmt.setString(2, "%"+keyword+"%");
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                String owner_email = rs.getString("owner_email");
+                String payee_email = rs.getString("payee_email");
+                double amount = rs.getDouble("amount");
+                int transaction_id = rs.getInt("transaction_id");
+                trans.add(new Transaction(transaction_id, amount, owner_email, payee_email, Integer.parseInt(accountId)));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return trans;
+    }
+
 }
